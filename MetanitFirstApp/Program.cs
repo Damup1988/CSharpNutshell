8c650ebd933533a5101286886e2acb346a1505bc
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-02-13 23:38:53 +0300

    Metanit - first app

---
+++ MetanitFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MetanitFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
